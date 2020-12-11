809690559af43e0b139cc8b7376736d9c1c6c62c
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-10 21:18:43 -0800

    updated seed data for milestones and age models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10 = Age.create(child_age: "10")
+1 = Age.create(child_age: "1")
+3 = Age.create(child_age: "3")
+
+Milestones.create(name:"Judah", notes:"lost tooth", image_url: "https://losttooth.jpg", age_id: 10.id) 
