f5e9abdc3c765f059093782cb88382f126a693b8
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-12 18:30:35 -0800

    create method and strong params

---
+++ app/controllers/api/v1/milestones_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::MilestonesController < ApplicationController
+    def index
+        @milestones = Milestone.all
+        render json: @milestones
+    end 
+
+    def create
+       milestones = Milestones.new(milestones_params)
+        if milestones.save
+            render json: milestones, status: :accepted
+        else
+            render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def milestones_params
+       params.require(milestones).permit(:name, :notes, :image_url, :age_id)
+    end 
+end
