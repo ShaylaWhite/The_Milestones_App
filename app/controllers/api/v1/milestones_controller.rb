a122c29fd6c75af4023a33664198f6cff115a7be
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-12 18:13:10 -0800

    index method

--- app/controllers/api/v1/milestones_controller.rb
+++ app/controllers/api/v1/milestones_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::MilestonesController < ApplicationController
+    def index
+        @milestones = Milestone.all
+        render json: @milestones
+    end 
+
+    def create
+
+    end 
 end
